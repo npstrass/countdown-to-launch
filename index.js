10766003a5fd94b89ade3f6636016b00d82e67b2
npstrass <npstrass@outlook.com>
2021-08-10 14:32:48 -0500

    updates

--- index.js
+++ index.js
@@ -45,25 +45,29 @@ function countTime() {
 }
 
 function printTime() {
-    // days.innerText = timeLeft.d
-    // hours.innerText = timeLeft.h
-    // minutes.innerText = timeLeft.m
-    // seconds.innerText = timeLeft.s
-
+    animateFlip(days, timeLeft.d)
+    animateFlip(hours, timeLeft.h)
+    animateFlip(minutes, timeLeft.m)
     animateFlip(seconds, timeLeft.s)
 }
 
 function animateFlip(element, value) {
-    element.querySelector('.top-back span').innerText = value    
-    element.querySelector('.bottom-back span').innerText = value  
+    const valueInDom = element.querySelector('.bottom-back').innerText
+    const currentValue = value < 10 ? '0' + value : '' + value
+    
+    if (valueInDom === currentValue) return
+
+    element.querySelector('.top-back span').innerText = currentValue    
+    element.querySelector('.bottom-back span').innerText = currentValue
     
     gsap.to(element.querySelector('.top'), 0.7, {
         rotationX: '-180deg',
         transformPerspective: 300,
         ease: Quart.easeOut,
-        onComplete: () => {
-            element.querySelector('.top').innerText = value
-            element.querySelector('.bottom').innerText = value
+        onComplete: function() {
+            element.querySelector('.top').innerText = currentValue
+            element.querySelector('.bottom').innerText = currentValue
+            gsap.set(element.querySelector('.top'), {rotationX: 0})
         }
     })
 
